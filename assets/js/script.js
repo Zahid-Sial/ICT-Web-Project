18466328da4b992c003b04f5d3d5310d18ea94d9
Zahid Mursleen <141985183+Zahid-Sial@users.noreply.github.com>
2024-11-14 07:15:55 +0500

    About me page is updated

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let heading_line = document.getElementById("heading-line");
+let background_img = document.getElementById("background-img");
+let page_link_btn = document.getElementsByClassName("page-link-btn");
+let explore_btn = document.getElementById("explore-btn");
+explore_btn.addEventListener("click",function(){
+    for (let i = 0; i < page_link_btn.length; i++) {
+        page_link_btn[i].style.display = 'block';
+      }
+    heading_line.style.display = "none";
+    background_img.style.display = "none";
+    explore_btn.style.display = "none";
+    console.log("Button Clicked");
+})
